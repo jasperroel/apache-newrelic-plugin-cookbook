b6a301a4727f59994d621b3caafe81305b0d9d5c
Jasper Roel <jroel@salesforce.com>
2015-12-09 18:42:12 -0800

    Bumped version to 0.1.4

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jasperroel@gmail.com'
 license 'all_rights'
 description 'Installs/Configures apache-newrelic-plugin'
 long_description 'Installs/Configures apache-newrelic-plugin'
-version '0.1.3'
+version '0.1.4'
 
 depends 'java'
